cc015bd4d9a726f286358b35c1606bfff57ae3cc
sus_tushhhh <tushant0430@gmail.com>
2026-04-10 17:42:16 +0530

    your

---
+++ testing/test.py
@@ -0,0 +1,17 @@
+import json
+import httpx
+import streamlit as st
+
+url = 'http://api.weatherapi.com/v1/forecast.json'
+
+params = {
+    'key' : st.secrets.weather_app.api_key,
+    'q' : 'Delhi India',
+    'dt' : '2026-03-27',
+    'aqi' : 'no'
+}
+
+r = httpx.get(url=url, params=params)
+
+with open('test.json', 'w') as f:
+    json.dump(r.json(), f, indent=4)
