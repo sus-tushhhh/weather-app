be61b60fcc4497be115a18371ee564852350c31a
sus_tushhhh <tushant0430@gmail.com>
2026-04-10 19:12:04 +0530

    main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title='Weather', layout='wide')
+st.set_page_config(page_title='Weather Lens', layout='wide')
+st.title('🌦️ Weather Lens | Forecast at a Glance')
 st.write('In Development')
