f04d7d522a55c0e176d01b814ecc0f67d98669c0
sus_tushhhh <tushant0430@gmail.com>
2026-04-15 18:27:05 +0530

    testings

--- main.py
+++ main.py
@@ -11,8 +11,19 @@ def get_base_locations():
     with open('base_locations.json', 'r') as f:
         return json.load(f)
 
+def load_weather():
+    location = st.session_state.get('location')
+    x = Weather(location)
+    x.get_response()
+    st.session_state['weather_data'] = x
 
 st.selectbox(
-    label    ='Enter a city, state or country',
-    options  = get_base_locations(),
+    label     = 'Enter a city, state or country',
+    options   = get_base_locations(),
+    on_change = load_weather,
+    key       = 'location'
 )
+
+
+if obj:=st.session_state.get('Weather'):
+    st.write(json.dumps(obj.location, indent=4))
